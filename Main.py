83f07000dee2e2de760d719d3ee88a64fe982803
Pensel <johannes.p.pensel@gmx.de>
2017-09-19 14:12:23 +0200

    babysteps

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-#todo: speedtest requests vs database
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.alternate.de/html/product/listingAboveFourtyPercent.html?lk=17979&size=500&sort=PRICEADVANTAGE&order=DESC&outlet=0&outlet=1#listingResult"
+
+def searchTerm():
+	return input("What do you want to search? ")
+
+def getLinks(url):
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, "html.parser")
+	listings = soup.find("div", {"id" : "listingResult"})
+
+def filterNames(listings):
+	pass
+
+def processNames(names):
+	pass
+
+
+
+if __name__ == "__main__":
