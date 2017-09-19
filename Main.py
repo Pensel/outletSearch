c061e145825c3526e72616cbe184659948bae31b
Pensel <johannes.p.pensel@gmx.de>
2017-09-19 16:31:26 +0200

    taking shape?

--- Main.py
+++ Main.py
@@ -9,17 +9,40 @@ def searchTerm():
 def processSearch(search):
 	pass
 
-def getLinks(url):
+def getSoup(url):
 	r = requests.get(url)
-	soup = BeautifulSoup(r.text, "html.parser")
-	listings = soup.find("div", {"id" : "listingResult"})
+	return BeautifulSoup(r.text, "html.parser")
 
-def filterNames(listings):
-	pass
+def getListings(soup):	
+	return soup.find("div", {"id" : "listingResult"})
 
+def filterNames(listings, page=0):
+	data = []
+	links = listings.findAll("a", {"class": "productLink"})
+	i = 0
+	for link in links:
+		data.append((i+page*500, link["title"], link["href"]))
+		i += 1
+	return data
+
+def completeOnePage(url, page):
+	return filterNames(getListings((getSoup(url))),page)
+
+
+def getAllPages(url):
+	soup = getSoup(url)
+	results = soup.find("span",{"class":"results"}).text
+	number = int(results[results.find("von ")+ 4:results.find(" Produkten")])
+	needed = number // 500
+	data = []
+	if needed > 0:
+		for i in range(0,needed +1):
+			data.append(completeOnePage("https://www.alternate.de/html/product/listingAboveFourtyPercent.html?lk=17979&page=" +str((i+1)) + "&size=500&sort=PRICEADVANTAGE&order=DESC&outlet=0&outlet=1#listingResult", i))
+	return data
 def processNames(names):
 	pass
 
 
 
 if __name__ == "__main__":
+	print(getAllPages(url))
